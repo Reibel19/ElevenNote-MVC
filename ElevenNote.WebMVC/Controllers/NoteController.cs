3ce7106745d4f3e1fc8dd6132d7c55da7edbf221
Luke <lukereibel@gmail.com>
2021-03-31 19:40:34 -0400

    adding notes stuff

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,40 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        // note/index
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+
+            return View(model);
+        }
+
+        // get: note/create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // post: note/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
+    }
+}
